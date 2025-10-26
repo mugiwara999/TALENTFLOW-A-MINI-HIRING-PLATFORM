d2baa2380b15388a8f43bf27ed00bd4d7e61222b
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2025-10-26 17:12:15 +0530

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Jobs from './pages/Jobs'
+import JobDetail from './pages/JobDetail'
+import Candidates from './pages/Candidates'
+import CandidateDetail from './pages/CandidateDetail'
+import Assessments from './pages/Assessments'
+import AssessmentBuilder from './pages/AssessmentBuilder'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <Routes>
+
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/jobs' element={<Jobs />}></Route>
+      <Route path='/jobs/:jobId' element={<JobDetail />}></Route>
+      <Route path='/candidates' element={<Candidates />}></Route>
+      <Route path='/candidates/:id' element={<CandidateDetail />}></Route>
+      <Route path='/assessments' element={<Assessments />}></Route>
+      <Route path='/assessments/new' element={<AssessmentBuilder />}></Route>
+      <Route path='/assessments/:id' element={<AssessmentBuilder />}></Route>
+
+    </Routes>
+
   )
+
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { initializeStorage } from './utils/storage'
+
+initializeStorage()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )

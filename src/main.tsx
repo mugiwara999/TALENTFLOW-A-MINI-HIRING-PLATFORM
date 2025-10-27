a3df9148fecdf309e2c5f37dbd3d6df4c9c64acf
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2025-10-27 19:02:41 +0530

    update app initialization

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,14 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { initializeStorage } from './utils/storage'
 
-initializeStorage()
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/browser')
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
+await initializeStorage()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

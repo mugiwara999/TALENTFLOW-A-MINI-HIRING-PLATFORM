0976d032480c117dbc99cbfcff3c131c30a5d4d3
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2025-10-27 14:17:21 +0530

    add Home Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import AboutUs from "../components/AboutUs";
+import Features from "../components/Features";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <Features />
+    </>
+  )
+}
